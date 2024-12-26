b06c5875ee30ca56b0228a2377b4688101727498
codematrics <codematricsitsolution@gmail.com>
2024-12-26 14:32:19 +0530

    some fix in navbar

--- src/data/navbar.js
+++ src/data/navbar.js
@@ -1,46 +1,130 @@
 export const navData = [
   {
-    name: "Services",
+    label: "Services",
     link: "services",
     subCategories: [
       {
-        name: "Full Stack development",
+        label: "Full Stack development",
         link: "full-stack-development",
       },
       {
-        name: "UI/UX design",
+        label: "UI/UX design",
         link: "ui-ux-design",
       },
       {
-        name: "Custom Software development",
+        label: "Custom Software development",
         link: "custom-software-development",
       },
       {
-        name: "Mobile App Development",
+        label: "Mobile App Development",
         link: "mobile-app-development",
       },
     ],
   },
   {
-    name: "Technologies",
+    label: "Technologies",
     link: "services",
     subCategories: [
       {
-        name: "Frontend",
-        link: "full-stack-development",
+        label: "Frontend",
+        link: "Frontend-development",
+        subCategories: [
+          {
+            label: "React",
+            link: "Frontend",
+          },
+          {
+            label: "Angular",
+            link: "Frontend",
+          },
+          {
+            label: "Vue",
+            link: "Frontend",
+          },
+          {
+            label: "NextJS",
+            link: "Frontend",
+          },
+        ],
       },
       {
-        name: "Backend",
-        link: "ui-ux-design",
+        label: "Backend",
+        link: "Backend-development",
+        subcategories: [
+          {
+            label: "Node.js",
+            link: "Backend",
+          },
+          {
+            label: "Express",
+            link: "Backend",
+          },
+          {
+            label: "Django",
+            link: "Backend",
+          },
+          {
+            label: "Flask",
+            link: "Backend",
+          },
+        ],
       },
       {
-        name: "Database",
+        label: "Database",
         link: "custom-software-development",
+        seubCategories: [
+          {
+            label: "MongoDB",
+            link: "custom-software-development",
+          },
+          {
+            label: "MySQL",
+            link: "custom-software-development",
+          },
+          {
+            label: "PostgreSQL",
+            link: "custom-software-development",
+          },
+          {
+            label: "Firebase",
+            link: "custom-software-development",
+          },
+        ],
       },
       {
-        name: "Mobile App Development",
+        label: "Mobile App Development",
         link: "mobile-app-development",
+        subcategories: [
+          {
+            label: "React Native",
+            link: "mobile-app-development",
+          },
+          {
+            label: "Flutter",
+            link: "mobile-app-development",
+          },
+          {
+            label: "Swift",
+            link: "mobile-app-development",
+          },
+          {
+            label: "Kotlin",
+            link: "mobile-app-development",
+          },
+        ],
       },
     ],
+  },
+  {
+    label: "Portfolio",
+    link: "portfolio",
+  },
+  {
+    label: "Blogs",
+    link: "Blogs",
+  },
+  {
+    label: "About",
+    link: "about-us",
   },
 ];
